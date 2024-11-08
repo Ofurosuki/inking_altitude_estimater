41deac25679cd8febf9263b98f91a04bf55a1c57
Ofururosuki <ryo-yoshida@keio.jp>
2024-11-08 10:43:18 +0900

    for sumitomo conference

--- estimate.py
+++ estimate.py
@@ -3,11 +3,13 @@ import pandas as pd
 import numpy as np
 import grid as grd
 import pickle as pkl
+import open3d as o3d
+import pandas as pd
 
-use_pickle = False
+use_pickle = True
 if use_pickle:
-    with open('point_cloud.pkl', 'rb') as f:
-        point_cloud = pkl.load(f)
+    with open('point_cloud_light.pkl', 'rb') as f:
+        points = pkl.load(f)
 else:
     csv_file = 'random_data.csv'  # use world frame data because the value fro waypoint is in world frame
     data = pd.read_csv(csv_file)
@@ -21,22 +23,20 @@ else:
 # Step 3: Create an Open3D PointCloud object
 point_cloud = o3d.geometry.PointCloud()
 point_cloud.points = o3d.utility.Vector3dVector(points)
-point_cloud.colors = o3d.utility.Vector3dVector(colors)
-
-# Remove points with z value under 0.3
-mask = points[:, 2] >= 0.3
-filtered_points = points[mask]
-filtered_colors = colors[mask]
-
-pcd_tree = o3d.geometry.KDTreeFlann(point_cloud)
-
-# Update the point cloud with filtered points and colors
-point_cloud.points = o3d.utility.Vector3dVector(filtered_points)
-point_cloud.colors = o3d.utility.Vector3dVector(filtered_colors)
-
-#load waypoints file
+point_cloud.colors = o3d.utility.Vector3dVector([0.5, 0.5, 0.5] for _ in range(len(points)))
+# Load waypoints file
 csv_waypoints = 'waypoints.csv'
 waypoints = pd.read_csv(csv_waypoints)
+waypoints_points = waypoints[['x', 'y', 'z']].values
+print(waypoints_points)
+
+# Create a point cloud of waypoints
+waypoints_cloud = o3d.geometry.PointCloud()
+waypoints_cloud.points = o3d.utility.Vector3dVector(waypoints_points)
+waypoints_cloud.colors = o3d.utility.Vector3dVector([1, 0, 0] for _ in range(len(waypoints_points)))
+
+
+# Step 4: Visualize the point cloud of waypoints
 assert set(waypoints.columns) == {'x', 'y', 'z'}, "CSV must contain x, y, z columns"
 waypoints_array = waypoints.values
 
@@ -50,27 +50,29 @@ print("Find its neighbors with distance less than 0.2, and paint them green.")
 
 
 unevenness = []
-def estimate_unevenness(pcd, k, radius):
+def estimate_unevenness(pcd, radius,target):
     pcd_tree = o3d.geometry.KDTreeFlann(pcd)
-    for i in range(len(pcd.points)):
-        [k, idx, _] = pcd_tree.search_radius_vector_3d(pcd.points[i], radius)
+    for i in range(len(target.points)):
+        if target.points[i][1] < -1.0:
+            radius = 0.1
+        [k, idx, _] = pcd_tree.search_radius_vector_3d(target.points[i], radius)
+        print("num:", len(idx))
+        np.asarray(pcd.colors)[idx[1:], :] = [0, 0, 1]
         average_z = np.mean(np.asarray(pcd.points)[idx[1:], 2])
-        unevenness.append(pcd.points[i][2] - average_z)
+        unevenness.append(average_z)
     return unevenness
 
 def write_to_csv(filename):
     data = pd.DataFrame(unevenness, columns=['unevenness'])
     data.to_csv(filename, index=False)
 
-# save to pickle
-with open('point_cloud.pkl', 'wb') as f:
-    pkl.dump(point_cloud, f)
 
+print(estimate_unevenness(point_cloud, 0.03, waypoints_cloud))
 
 # Step 4: Visualize the point cloud
-grid=grd.gen_grid(1, 10)
-coordinates_frame=o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0,0,0])
-o3d.visualization.draw_geometries([point_cloud, grid, coordinates_frame])
+grid=grd.gen_grid(0.1, 10)
+coordinates_frame=o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0,0,0])
+o3d.visualization.draw_geometries([point_cloud, grid, coordinates_frame,waypoints_cloud])
 
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import open3d as o3d
 import pickle as pkl
+import grid as grd
+import numpy as np
 
 
-use_pickle = False
+use_pickle = True
 if use_pickle:
     with open('point_cloud.pkl', 'rb') as f:
         points = pkl.load(f)
@@ -18,6 +20,9 @@ else:
     points = data[['x', 'y', 'z']].values
     colors = data[['R', 'G', 'B']].values   # Normalize color values to [0, 1]
 
+    # Remove points with z value more than 0
+    points = points[points[:, 2] <= -10]
+    
 
     # Remove points with x value less than -500
     #points = points[points[:, 1] >= 500]
@@ -25,14 +30,23 @@ else:
     #colors = colors[points[:, 0] >= -500]
     # Switch points' x and y
     #points[:, [0, 1]] = points[:, [1, 0]] #lefthanded to rightha
+    
+
 
     with open('point_cloud.pkl', 'wb') as f:
         pkl.dump(points, f)
 
 # Step 3: Create an Open3D PointCloud object
+points = points[points[:, 2] <= -0.3]
+translation = np.array([0, 0, 0.70])
+points += translation
+with open('point_cloud_light.pkl', 'wb') as f:
+        pkl.dump(points, f)
+
 point_cloud = o3d.geometry.PointCloud()
 point_cloud.points = o3d.utility.Vector3dVector(points)
 point_cloud.colors = o3d.utility.Vector3dVector([0.5, 0.5, 0.5] for _ in range(len(points)))
 # Step 4: Add coordinate axes to the point cloud
-#coordinate_axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0])
-o3d.visualization.draw_geometries([point_cloud])  # Visualize the point cloud
+grid = grd.gen_grid(0.1, 10)
+coordinate_axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0])
+o3d.visualization.draw_geometries([point_cloud, coordinate_axes, grid])  # Visualize the point cloud
